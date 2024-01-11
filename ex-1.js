ad4de381f7f352a679b4b24087d02b3df60485de
Palm <palml1lax@gmail.com>
2024-01-12 01:58:30 +0700

    feat: finish ex

--- ex-1.js
+++ ex-1.js
@@ -2,6 +2,12 @@
 
 const lengthOfLastWord = function (s) {
   //Start coding here
+  const str = s.trim().split("").reverse();
+  for (let i = 0; i <= str.length - 1; i++) {
+    if (str[i] === " ") {
+      return str.slice(0, i).length;
+    }
+  }
 };
 
 const result1 = lengthOfLastWord("Hello World");
@@ -11,3 +17,25 @@ const result3 = lengthOfLastWord("luffy is still joyboy");
 console.log(result1); // 5
 console.log(result2); // 4
 console.log(result3); // 6
+
+// const lengthOfLastWord = function (s) {
+//   //Start coding here
+//   const str = s.split(" ");
+//   let result;
+//   console.log(str);
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     if (str[i] !== "") {
+//       result = str[i];
+//       console.log(result)
+//       return result.length;
+//     }
+//   }
+// };
+
+// const result1 = lengthOfLastWord("Hello World");
+// const result2 = lengthOfLastWord("   fly me   to   the moon  ");
+// const result3 = lengthOfLastWord("luffy is still joyboy");
+
+// console.log(result1); // 5
+// console.log(result2); // 4
+// console.log(result3); // 6
--- ex-2.js
+++ ex-2.js
@@ -2,12 +2,36 @@
 
 const singleNumber = function (nums) {
   // Start coding here
+  const checknums = nums.sort((a, b) => a - b);
+
+  const checked = [];
+  for (let i = 0; i <= checknums.length - 1; i++) {
+    if (
+      checknums[i - 1] !== checknums[i] &&
+      checknums[i] !== checknums[i + 1]
+    ) {
+      checked.push(checknums[i]);
+    }
+  }
+  if (checked.length !== 1) {
+    return checked;
+  } else {
+    return checked[0];
+  }
 };
 
 const result1 = singleNumber([2, 2, 1]);
 const result2 = singleNumber([4, 1, 2, 1, 2]);
 const result3 = singleNumber([1]);
 
+const result4 = singleNumber([5, 7, 5, 9, 8, 1, 8, 8, 8, 9, 10]);
+
 console.log(result1); // 1
 console.log(result2); // 4
 console.log(result3); // 1
+
+console.log(result4);
+
+
+
+
